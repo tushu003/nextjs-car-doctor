18e73df5aaae2ee38f6f69f75ff0ee3879b641ca
tushu003 <tahminata2015@gmail.com>
2025-11-08 11:31:25 +0600

    first commit

---
+++ src/app/components/NavBar.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import Link from 'next/link'
+import tailwindcss from 'tailwindcss'
+import Image from 'next/image'
+import logo from '../../../public/assets/icons/logo.png'
+
+export default function NavBar() {
+  return (
+    <div>
+      <div className="navbar bg-base-100 shadow-sm">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+      </div>
+       
+      <ul
+        tabIndex="-1"
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+        <li><a>Item 1</a></li>
+        <li>
+          <a>Parent</a>
+          <ul className="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </li>
+        <li><a>Item 3</a></li>
+      </ul>
+    </div>
+     <Image src={logo} alt="logo" width={60} height={60} />
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <li><a>Item 1</a></li>
+      <li>
+        <details>
+          <summary>Parent</summary>
+          <ul className="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </details>
+      </li>
+      <li><a>Item 3</a></li>
+    </ul>
+  </div>
+  <div className="navbar-end">
+    <a className="btn">Button</a>
+  </div>
+</div>
+    </div>
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+// tailwind.config.js
+import daisyui from 'daisyui'
+
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      }
+    }
+  },
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["light"], // first theme in the array is used as default unless overridden
+    // optional: set darkTheme name if needed
+    // darkTheme: "dark",
+  },
+}
+
+export default config
