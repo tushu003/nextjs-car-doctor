5d14ea316fff83257677fa8342afe9d109a5c812
tushu003 <tahminata2015@gmail.com>
2025-11-09 20:59:26 +0600

    add social icon in footer

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import React from 'react';
 import logo from '../../../public/assets/icons/logo.png'
+import { FaGoogle, FaFacebook, FaTwitter, FaInstagram, FaLinkedin } from 'react-icons/fa';
 
 export default function Footer() {
   return (
@@ -8,11 +9,11 @@ export default function Footer() {
         <footer className="footer sm:footer-horizontal bg-[#151515] mt-8 text-base-content p-10">
   <aside>
 <Image src={logo}  alt='logo' width={70} height={70}/>
-    <p className='text-white'>
-      ACME Industries Ltd.
-      <br />
-      Providing reliable tech since 1992
-    </p>
+    <p className='text-white/80'>
+    Edwin Diaz is a software and web <br /> technologies engineer, a life coach <br /> trainer who is also a serial</p>
+    <div className='flex items-center gap-2 mt-4'>
+       <FaGoogle className='text-white w-4 h-4' /> / <FaFacebook className='text-white w-4 h-4' /> / <FaTwitter className='text-white w-4 h-4' /> / <FaInstagram className='text-white w-4 h-4' /> / <FaLinkedin className='text-white w-4 h-4' />
+    </div>
   </aside>
   <nav>
     <h6 className="footer-title text-white">Services</h6>
