6e23016bcc28bcea30b3e2579b664b1138cf110d
tushu003 <tahminata2015@gmail.com>
2025-11-10 21:21:19 +0600

    start services section and show data on ui

---
+++ src/app/components/Services.jsx
@@ -0,0 +1,199 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function Services() {
+    const data = [
+        {
+            "_id": "635a0c0b64a6d231228942ae",
+            "service_id": "04",
+            "title": "Engine Oil Change",
+            "img": "https://i.ibb.co/T2cpBd5/888.jpg",
+            "price": "20.00",
+            "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veniam nostrum dolores nemo quas. Minima ullam, veniam, nesciunt quae dolore animi blanditiis deserunt, ea esse dolorum ipsum quibusdam ipsa! Corrupti at, excepturi, fugiat aut nihil neque aliquid sapiente dignissimos provident, animi molestiae ipsum. Repudiandae ipsa id nihil reiciendis soluta eos ducimus pariatur, nam architecto tenetur quo quos commodi est libero repellendus vitae. Fuga numquam nulla nam, facere neque expedita voluptatibus pariatur necessitatibus vel, dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum. Aspernatur ex quibusdam at cum nulla!",
+            "facility": [
+                {
+                    "name": "Instant Car Services",
+                    "details": "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum."
+                },
+                {
+                    "name": "24/7 Quality Service",
+                    "details": "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum."
+                },
+                {
+                    "name": "Easy Customer Service",
+                    "details": "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum."
+                },
+                {
+                    "name": "Quality Cost Service",
+                    "details": "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum."
+                }
+            ]
+        },
+        {
+            "_id": "635a0c0b64a6d231228942af",
+            "service_id": "05",
+            "title": "Battery Charge",
+            "img": "https://i.ibb.co/ydCbDN3/5555.jpg",
+            "price": "20.00",
+            "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veniam nostrum dolores nemo quas. Minima ullam, veniam, nesciunt quae dolore animi blanditiis deserunt, ea esse dolorum ipsum quibusdam ipsa! Corrupti at, excepturi, fugiat aut nihil neque aliquid sapiente dignissimos provident, animi molestiae ipsum. Repudiandae ipsa id nihil reiciendis soluta eos ducimus pariatur, nam architecto tenetur quo quos commodi est libero repellendus vitae. Fuga numquam nulla nam, facere neque expedita voluptatibus pariatur necessitatibus vel, dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum. Aspernatur ex quibusdam at cum nulla!",
+            "facility": [
+                {
+                    "name": "Instant Car Services",
+                    "details": "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum."
+                },
+                {
+                    "name": "24/7 Quality Service",
+                    "details": "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum."
+                },
+                {
+                    "name": "Easy Customer Service",
+                    "details": "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum."
+                },
+                {
+                    "name": "Quality Cost Service",
+                    "details": "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum."
+                }
+            ]
+        },
+        {
+            "_id": "635b591a1dafe382a9da8c96",
+            "service_id": "01",
+            "title": "Full car Repair",
+            "img": "https://i.ibb.co/R6Z2nFM/55.jpg",
+            "price": "200.00",
+            "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veniam nostrum dolores nemo quas. Minima ullam, veniam, nesciunt quae dolore animi blanditiis deserunt, ea esse dolorum ipsum quibusdam ipsa! Corrupti at, excepturi, fugiat aut nihil neque aliquid sapiente dignissimos provident, animi molestiae ipsum. Repudiandae ipsa id nihil reiciendis soluta eos ducimus pariatur, nam architecto tenetur quo quos commodi est libero repellendus vitae. Fuga numquam nulla nam, facere neque expedita voluptatibus pariatur necessitatibus vel, dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum. Aspernatur ex quibusdam at cum nulla!",
+            "facility": [
+                {
+                    "name": "Instant Car Services",
+                    "details": "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum."
+                },
+                {
+                    "name": "24/7 Quality Service",
+                    "details": "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum."
+                },
+                {
+                    "name": "Easy Customer Service",
+                    "details": "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum."
+                },
+                {
+                    "name": "Quality Cost Service",
+                    "details": "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum."
+                }
+            ]
+        },
+        {
+            "_id": "635b5afc1dafe382a9da8c98",
+            "service_id": "02",
+            "title": "Engine Repair",
+            "img": "https://i.ibb.co/5MvmD2g/88.jpg",
+            "price": "150.00",
+            "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veniam nostrum dolores nemo quas. Minima ullam, veniam, nesciunt quae dolore animi blanditiis deserunt, ea esse dolorum ipsum quibusdam ipsa! Corrupti at, excepturi, fugiat aut nihil neque aliquid sapiente dignissimos provident, animi molestiae ipsum. Repudiandae ipsa id nihil reiciendis soluta eos ducimus pariatur, nam architecto tenetur quo quos commodi est libero repellendus vitae. Fuga numquam nulla nam, facere neque expedita voluptatibus pariatur necessitatibus vel, dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum. Aspernatur ex quibusdam at cum nulla!",
+            "facility": [
+                {
+                    "name": "Instant Car Services",
+                    "details": "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum."
+                },
+                {
+                    "name": "24/7 Quality Service",
+                    "details": "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum."
+                },
+                {
+                    "name": "Easy Customer Service",
+                    "details": "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum."
+                },
+                {
+                    "name": "Quality Cost Service",
+                    "details": "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum."
+                }
+            ]
+        },
+        {
+            "_id": "635b5b691dafe382a9da8c99",
+            "service_id": "03",
+            "title": "Automatic Services",
+            "img": "https://i.ibb.co/wh7t3N3/555.jpg",
+            "price": "30.00",
+            "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veniam nostrum dolores nemo quas. Minima ullam, veniam, nesciunt quae dolore animi blanditiis deserunt, ea esse dolorum ipsum quibusdam ipsa! Corrupti at, excepturi, fugiat aut nihil neque aliquid sapiente dignissimos provident, animi molestiae ipsum. Repudiandae ipsa id nihil reiciendis soluta eos ducimus pariatur, nam architecto tenetur quo quos commodi est libero repellendus vitae. Fuga numquam nulla nam, facere neque expedita voluptatibus pariatur necessitatibus vel, dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum. Aspernatur ex quibusdam at cum nulla!",
+            "facility": [
+                {
+                    "name": "Instant Car Services",
+                    "details": "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum."
+                },
+                {
+                    "name": "24/7 Quality Service",
+                    "details": "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum."
+                },
+                {
+                    "name": "Easy Customer Service",
+                    "details": "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum."
+                },
+                {
+                    "name": "Quality Cost Service",
+                    "details": "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum."
+                }
+            ]
+        },
+        {
+            "_id": "635b5ba51dafe382a9da8c9a",
+            "service_id": "06",
+            "title": "Electrical System",
+            "img": "https://i.ibb.co/KzCG8qr/8888.jpg",
+            "price": "20.00",
+            "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius veniam nostrum dolores nemo quas. Minima ullam, veniam, nesciunt quae dolore animi blanditiis deserunt, ea esse dolorum ipsum quibusdam ipsa! Corrupti at, excepturi, fugiat aut nihil neque aliquid sapiente dignissimos provident, animi molestiae ipsum. Repudiandae ipsa id nihil reiciendis soluta eos ducimus pariatur, nam architecto tenetur quo quos commodi est libero repellendus vitae. Fuga numquam nulla nam, facere neque expedita voluptatibus pariatur necessitatibus vel, dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum. Aspernatur ex quibusdam at cum nulla!",
+            "facility": [
+                {
+                    "name": "Instant Car Services",
+                    "details": "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum."
+                },
+                {
+                    "name": "24/7 Quality Service",
+                    "details": "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum."
+                },
+                {
+                    "name": "Easy Customer Service",
+                    "details": "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum."
+                },
+                {
+                    "name": "Quality Cost Service",
+                    "details": "Fuga numquam nulla nam, facere neque dignissimos ab esse magni accusamus eveniet ad corrupti, architecto nostrum."
+                }
+            ]
+        }
+    ]
+    
+  return (
+    <div className='grid grid-cols-12'>
+        {
+            data.map((item)=>(
+                <article
+            key={item._id}
+            className="bg-white rounded-lg shadow-md overflow-hidden col-span-4"
+          >
+            <div className="relative w-full h-40">
+              <Image
+                src={item.img}
+                alt={item.title}
+                fill
+                className="object-cover"
+                // remove `unoptimized` in production if you whitelist domains in next.config.js
+                unoptimized
+              />
+            </div>
+  
+            <div className="p-4 flex-1 flex flex-col">
+              <h3 className="text-lg font-semibold">{item.title}</h3>
+              <p className="text-sm text-gray-500 mt-2 line-clamp-3">{item.description}</p>
+  
+              <div className="mt-4 flex items-center justify-between">
+                <span className="text-red-600 font-bold">${item.price}</span>
+                <button className="px-3 py-1 bg-red-500 text-white rounded-md text-sm">
+                  Book Now
+                </button>
+              </div>
+            </div>
+          </article>
+            ))};
+    </div>
+  );
+            
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,12 +2,14 @@ import Image from "next/image";
 import Banner from "./components/Banner";
 import About from "./components/About";
 import InfoStrip from "./components/InfoStrip";
+import Services from "./components/Services";
 
 export default function Home() {
   return (
   <div>
     <Banner />
     <About />
+    <Services />
     <InfoStrip />
   </div>
   );
