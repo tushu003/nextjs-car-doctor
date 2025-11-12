e450fed1e354debd8b75d6cb9c3f27587e7e8c9e
tushu003 <tahminata2015@gmail.com>
2025-11-12 18:42:57 +0600

    get data on mongodb

--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -1,5 +1,5 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
-const uri = "mongodb+srv://<db_username>:<db_password>@cluster0.jlzgpsq.mongodb.net/?appName=Cluster0";
+const uri = "mongodb+srv://services-user:C6o79Rc4DdTRJ2d9@cluster0.jlzgpsq.mongodb.net/?appName=Cluster0";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 export default function dbConnect(collectionName){
     const client = new MongoClient(uri, {
@@ -9,7 +9,7 @@ export default function dbConnect(collectionName){
           deprecationErrors: true,
         }
       });
-      return client.db(services).collection(collectionName)
+      return client.db("services").collection(collectionName)
 }
 
 
