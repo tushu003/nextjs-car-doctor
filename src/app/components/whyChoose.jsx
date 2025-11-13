f222706f4e71428ebe2b548f1912b6cab9581225
tushu003 <tahminata2015@gmail.com>
2025-11-13 22:25:45 +0600

    modify card on why choose us section

--- src/app/components/whyChoose.jsx
+++ src/app/components/whyChoose.jsx
@@ -9,34 +9,34 @@ import group5 from "../../../public/assets/icons/Group 38729.png";
 
 export default function WhyChoose() {
   return (
-    <div>
-            <p className='text-[#FF3811] font-bold text-center mt-6'>service</p>
+    <div className='mb-10'>
+            <p className='text-[#FF3811] font-bold text-center mt-10'>service</p>
         <h2 className='text-black font-bold text-center text-2xl mb-1'>Our Service Area</h2>
         <p className='text-center text-gray-600 mb-6'>The majority have suffered alteration in some form, by injected humour, or randomised <br /> words which do not look even slightly believable. </p>
-        <div className='grid grid-cols-3 lg:grid-cols-6 gap-4'>
-            <div className='p-6 rounded-[4px] border border-gray-200'>
-                <Image src={group} alt='group' width={24} height={34} />
-                <p>Expert Team</p>
+        <div className='grid grid-cols-3 lg:grid-cols-6 gap-4 mb-8'>
+            <div className='p-6 rounded-[4px] border border-gray-200 text-center items-center'>
+                <Image src={group} alt='group' width={28} height={28} className='my-1 ml-10 ' />
+                <p className='text-black font-bold text-center text-[18px] mb-1'>Expert Team</p>
             </div>
             <div className='p-6 rounded-[4px] bg-[#FF3811] border border-gray-200'>
-                <Image src={group5} alt='group' width={24} height={34} />
-                <p>Expert Team</p>
+                <Image src={group5} alt='group' width={28} height={28} className='my-1 ml-10' />
+                <p className='text-black font-bold text-center text-[18px] mb-1'>Expert Team</p>
             </div>
             <div className='p-6 rounded-[4px] border border-gray-200'>
-                <Image src={group4} alt='group' width={24} height={34} />
-                <p>Expert Team</p>
+                <Image src={group4} alt='group' width={28} height={28} className='my-1 ml-10' />
+                <p className='text-black font-bold text-center text-[18px] mb-1'>Expert Team</p>
             </div>
             <div className='p-6 rounded-[4px] border border-gray-200'>
-                <Image src={group3} alt='group' width={24} height={34} />
-                <p>Expert Team</p>
+            <Image src={group3} alt='group' width={28} height={28} className='my-1 ml-10' />
+                <p className='text-black font-bold text-center text-[18px] mb-1'>Expert Team</p>
             </div>
             <div className='p-6 rounded-[4px] border border-gray-200'>
-                <Image src={group1} alt='group' width={24} height={34} />
-                <p>Expert Team</p>
+                <Image src={group1} alt='group' width={28} height={28} className='my-1 ml-10' />
+                <p className='text-black font-bold text-center text-[18px] mb-1'>Expert Team</p>
             </div>
             <div className='p-6 rounded-[4px] border border-gray-200'>
-                <Image src={group2} alt='group' width={24} height={34} />
-                <p>Expert Team</p>
+                <Image src={group2} alt='group' width={28} height={28} className='my-1 ml-10' />
+                <p className='text-black font-bold text-center text-[18px] mb-1'>Expert Team</p>
             </div>
         </div>
     </div>
