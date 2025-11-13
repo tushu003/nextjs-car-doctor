d1b400000edb7d62442d0485d62acd80a5929a8a
tushu003 <tahminata2015@gmail.com>
2025-11-13 22:27:45 +0600

    text changes

--- src/app/components/whyChoose.jsx
+++ src/app/components/whyChoose.jsx
@@ -10,8 +10,8 @@ import group5 from "../../../public/assets/icons/Group 38729.png";
 export default function WhyChoose() {
   return (
     <div className='mb-10'>
-            <p className='text-[#FF3811] font-bold text-center mt-10'>service</p>
-        <h2 className='text-black font-bold text-center text-2xl mb-1'>Our Service Area</h2>
+            <p className='text-[#FF3811] font-bold text-center mt-10'>Core Features</p>
+        <h2 className='text-black font-bold text-center text-2xl mb-1'>Why Choose Us</h2>
         <p className='text-center text-gray-600 mb-6'>The majority have suffered alteration in some form, by injected humour, or randomised <br /> words which do not look even slightly believable. </p>
         <div className='grid grid-cols-3 lg:grid-cols-6 gap-4 mb-8'>
             <div className='p-6 rounded-[4px] border border-gray-200 text-center items-center'>
