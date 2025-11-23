113f8b09ccb6453d178f184f22b9673b1c88db64
tushu003 <tahminata2015@gmail.com>
2025-11-23 14:15:00 +0600

    chnage hover color

--- src/app/components/Products.jsx
+++ src/app/components/Products.jsx
@@ -103,7 +103,7 @@ export default function Products() {
       </div>
 
       <div className="mt-8 text-center">
-        <button className="inline-block px-3 py-2 rounded-8[px] border border-[#FF3811] text-[#FF3811] text-sm hover:bg-red-50 transition">
+        <button className="inline-block px-3 py-2 rounded-8[px] border border-[#FF3811] text-[#FF3811] text-sm hover:bg-[#FF3811] transition">
           More Products
         </button>
       </div>
